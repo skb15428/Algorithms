ed3a96903d3bb8f68b87ab25262b2b53424ba292
Saurabh Kiran Baviskar <skb15428@gmail.com>
2021-01-18 20:14:55 +0530

    Update Modulo Multiplicative Inverse.cpp

--- Number Theory/Modulo Multiplicative Inverse.cpp
+++ Number Theory/Modulo Multiplicative Inverse.cpp
@@ -30,12 +30,52 @@ ll power(ll base, ll exp, ll md=mod)
 ////////////////////////////////////Fermit's Little Theorem - O(log(m))////////////////////////////////
 
 // a^(m-2) = a^(-1) mod m
-ll moduloInverse(ll a, ll m)
+ll inverse_mod_fer(ll a, ll m)
 {
     return power(a, m - 2, m);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////Extended Euclidean Algo - O(log(n))///////////////////////////
+
+ll ext_gcd(ll a, ll b, ll &x, ll &y)
+{
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    };
+    ll x1, y1;
+    ll d = ext_gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////Modular Multiplicative Inverse using extended Euclidean algo/////////////////
+
+ll inverse_mod_ext(ll a, ll m)
+{
+    ll x, y;
+    ll g = ext_gcd(a, m, x, y);
+    if (g != 1)
+    {
+        cout << "No solution!";
+        return -1;
+    }
+    else
+    {
+        x = (x % m + m) % m;
+        return x;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main()
 {
     FIO;
@@ -46,7 +86,8 @@ int main()
     {
         ll a, m;
         cin >> a >> m;
-        cout << moduloInverse(a, m) << endl;
+        cout << inverse_mod_fer(a, m) << endl;
+        cout << inverse_mod_ext(a, m) << endl;
     }
     return 0;
 }
