61d58f1df08a48361d3fbef354b19c7b02aaffd1
Saurabh Kiran Baviskar <skb15428@gmail.com>
2021-03-02 09:46:18 +0530

    Update Coin Change.cpp

--- Dynamic Programming/Coin Change.cpp
+++ Dynamic Programming/Coin Change.cpp
@@ -61,6 +61,9 @@ int coinChange_SO(vi &coin, int n, int sum)
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Given a value N, if we want to make change for N cents, and we have infinite supply of each 
+// of S = { S1, S2, .. , Sm} valued coins, how many ways can we make the change? The order of coins
+// doesn’t matter.
 int main()
 {
     FIO;
