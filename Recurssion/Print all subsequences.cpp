0a8987519096b6c84f21891e450b0eb58e1e1b06
skb15428 <skb15428@gmail.com>
2023-07-10 22:37:49 +0530

    Print all subsequence using recurssion

---
+++ Recurssion/Print all subsequences.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// Method 1
+void printAllSequences(vector<int> &arr, vector<int> &ans, int x)
+{
+    print(ans);
+    for (int i = x; i < arr.size(); i++)
+    {
+        ans.push_back(arr[i]);
+        printAllSequences(arr, ans, i + 1);
+        ans.pop_back();
+    }
+}
+
+// Method 2
+void printAllSequences2(vector<int> &arr, vector<int> &ans, int x)
+{
+    if (x >= arr.size())
+    {
+        print(ans);
+        return;
+    }
+
+    ans.push_back(arr[x]);
+    printAllSequences2(arr, ans, x + 1);
+    ans.pop_back();
+    printAllSequences2(arr, ans, x + 1);
+}
+
+// Time Complexity - O(2^n)
+// Space Complexity - O(2^n)
+int main()
+{
+    vector<int> arr;
+    arr.push_back(3);
+    arr.push_back(1);
+    arr.push_back(2);
+
+    vector<int> ans;
+    printAllSequences(arr, ans, 0);
+    printAllSequences2(arr, ans, 0);
+    return 0;
+}
